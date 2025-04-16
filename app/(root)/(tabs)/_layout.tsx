91b462b33168c2fc10d3c9a843147ccbcc44ef60
Akinola Akinbusola <akinbusolaakinola2@gmail.com>
2025-04-16 19:49:13 +0100

    Bottom Tab Bar

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,38 +1,71 @@
 import { Tabs } from "expo-router";
 import { icons } from "@/constants";
-import { Image } from "react-native";
+import { Image, StyleSheet } from "react-native";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ tabBarShowLabel: false }}>
+    <Tabs screenOptions={{ 
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+            backgroundColor: '#333333',
+            borderRadius: 50,
+            paddingTop: 10,
+            overflow: "hidden",
+            marginHorizontal: 20,
+            marginBottom: 20,
+            height: 70,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'row',
+            position: "absolute"
+        },
+        }}
+    >
       <Tabs.Screen 
         name="home" 
         options={{
           title: "Home",
-          tabBarIcon: ({ color }:any) => <Image source={icons.home} style={{ width: 24, height: 24, tintColor: color }} />
+          tabBarIcon: ({ color }:any) => (
+            <Image source={icons.home} style={[styles.iconStyle, { tintColor: color }]} />
+          )
         }}
       />
       <Tabs.Screen 
         name="rides" 
         options={{
           title: "Rides",
-          tabBarIcon: ({ color }:any) => <Image source={icons.list} style={{ width: 24, height: 24, tintColor: color }} />
+          tabBarIcon: ({ color }:any) => (
+            <Image source={icons.list} style={[styles.iconStyle, { tintColor: color }]} />
+          )
         }}
       />
       <Tabs.Screen 
         name="chat" 
         options={{
           title: "Chat",
-          tabBarIcon: ({ color }:any) => <Image source={icons.chat} style={{ width: 24, height: 24, tintColor: color }} />
+          tabBarIcon: ({ color }:any) => (
+            <Image source={icons.chat} style={[styles.iconStyle, { tintColor: color }]} />
+          )
         }}
       />
       <Tabs.Screen 
         name="profile" 
         options={{
           title: "Profile",
-          tabBarIcon: ({ color }:any) => <Image source={icons.profile} style={{ width: 24, height: 24, tintColor: color }} />
+          tabBarIcon: ({ color }:any) => (
+            <Image source={icons.profile} style={[styles.iconStyle, { tintColor: color }]} />
+          )
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  iconStyle: {
+    width: 24,
+    height: 24,
+  }
+});
